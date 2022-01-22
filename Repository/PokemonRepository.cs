9b4b649b076bccae4ec4fce94b62c25c397375ed
Kingsley Brown <kingsleybrownjr@yahoo.com>
2022-01-22 12:15:33 -0600

    Added more methods to the PokemonRepository to query the database to get reviews or a specific pokemon

--- Repository/PokemonRepository.cs
+++ Repository/PokemonRepository.cs
@@ -13,9 +13,34 @@ namespace PokemonReviewApp.Repository
             _context = context;
         }
 
+        public Pokemon GetPokemon(int id)
+        {
+            return _context.Pokemon.Where(pokemon => pokemon.Id == id).FirstOrDefault();
+        }
+
+        public Pokemon GetPokemon(string name)
+        {
+            return _context.Pokemon.Where(pokemon =>pokemon.Name == name).FirstOrDefault();
+        }
+
+        public decimal GetPokemonRating(int id)
+        {
+            var review = _context.Reviews.Where(pokemon => pokemon.Id != id);
+
+            if(review.Count() <= 0)
+                return 0;
+
+            return ((decimal)review.Sum(review => review.Rating) / review.Count());
+        }
+
         public ICollection<Pokemon> GetPokemons()
         {
             return _context.Pokemon.OrderBy(pokemon => pokemon.Id).ToList();
+        }
+
+        public bool PokemonExists(int id)
+        {
+            return _context.Pokemon.Any(pokemon => pokemon.Id == id);
         }
     }
 }
