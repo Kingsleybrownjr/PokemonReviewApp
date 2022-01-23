46c3e70b0c25847a27233287a6125022ad805157
Kingsley Brown <kingsleybrownjr@yahoo.com>
2022-01-23 11:58:19 -0600

    added ownerdto,get requests and repository

---
+++ Repository/OwnerRepository.cs
@@ -0,0 +1,40 @@
+using PokemonReviewApp.Data;
+using PokemonReviewApp.Interfaces;
+using PokemonReviewApp.Models;
+
+namespace PokemonReviewApp.Repository
+{
+    public class OwnerRepository : IOwnerRepository
+    {
+        private readonly DataContext _context;
+        public OwnerRepository(DataContext context)
+        {
+            _context = context;
+        }
+
+        public Owner GetOwner(int ownerId)
+        {
+            return _context.Owners.Where(owner => owner.Id == ownerId).FirstOrDefault();
+        }
+
+        public ICollection<Owner> GetOwnerOfAPokemon(int pokeId)
+        {
+            return _context.PokemonOwners.Where(p => p.Pokemon.Id == pokeId).Select(owner => owner.Owner).ToList();
+        }
+
+        public ICollection<Owner> GetOwners()
+        {
+            return _context.Owners.ToList();
+        }
+
+        public ICollection<Pokemon> GetPokemonByOwner(int ownerId)
+        {
+            return _context.PokemonOwners.Where(pokemon => pokemon.Owner.Id == ownerId).Select(p => p.Pokemon).ToList();
+        }
+
+        public bool OwnerExists(int ownerId)
+        {
+            return _context.Owners.Any(owner => owner.Id == ownerId);
+        }
+    }
+}
